607de03355bc73a6b4116720ba8afa2f2ec4c2a9
Sebastián Araya <arayariquelmes@gmail.com>
2020-11-26 06:21:48 -0300

    Ruta service worker

--- sw.js
+++ sw.js
@@ -7,17 +7,17 @@ const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable=v1";
 
 const APP_SHELL = [
-    "/",
-    '/index.html',
-    "/css/style.css",
-    '/img/favicon.ico',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
-    '/js/app.js',
-    '/js/sw-utils.js'
+    //"/",
+    'index.html',
+    "css/style.css",
+    'img/favicon.ico',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
 ];
 
 //Lo que no se modificara jamas
